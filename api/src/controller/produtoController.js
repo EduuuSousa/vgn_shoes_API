b1b7ef4b01b40d2a46a619fcc375577933ad6e8f
Matheus Dos Santos Andrade <129523267+SRANDRADE18@users.noreply.github.com>
2023-11-16 00:23:40 -0300

    .

--- api/src/controller/produtoController.js
+++ api/src/controller/produtoController.js
@@ -4,6 +4,50 @@ import { CadastrarProduto } from "../repository/produto/produtoRepository.js";
 
 const endpoint = Router();
 
+
+endpoint.post('/cadastrar-produto', async (req,resp) => {
+
+    try {
+        const novoProduto=req.body;
+
+        if (!novoProduto.nome)
+        throw new Error('Nome do Produto é obrigatorio!');
+
+        if (!novoProduto.preco)
+        throw new Error('preço do Produto é obrigatorio!');
+
+        if (!novoProduto.avaliacao)
+        throw new Error('avalicao do Produto é obrigatorio!');
+
+        if (!novoProduto.genero)
+        throw new Error('genero do Produto é obrigatorio!');
+
+        if (!novoProduto.estoque)
+        throw new Error('estoque do Produto é obrigatorio!');
+
+        if (!novoProduto.disponivel)
+        throw new Error('disponivel do Produto é obrigatorio!');
+
+        if (!novoProduto.Descricao)
+        throw new Error('Descricao do Produto é obrigatorio!');
+
+        if (!novoProduto.Forro)
+        throw new Error('Forro do Produto é obrigatorio!');
+
+        if (!novoProduto.solado)
+        throw new Error('solado do Produto é obrigatorio!');
+
+        if (!novoProduto.palmilha)
+        throw new Error('palmilha do Produto é obrigatorio!');
+
+
+    } catch (error) {
+        resp.status(400).send({
+            error: error.message
+        })
+    }
+});
+
 endpoint.post('/cadastrar-produto' ,async (req, resp) => {
     try {
         const produto = req.body;
